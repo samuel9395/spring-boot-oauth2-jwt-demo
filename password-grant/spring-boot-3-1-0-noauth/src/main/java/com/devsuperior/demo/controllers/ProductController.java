6bfeeabd9e7bdefe18c8718105cfbc3f847ccfac
Samuel Bitencourt <samuelb2f@outlook.com>
2026-07-01 22:52:07 -0300

    sec(auth): #validacao_e_seguranca "access control by role, by route"

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/controllers/ProductController.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/controllers/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,18 +24,37 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 
+    // Em ResourceServerConfig, a configuração global por padrão está permitindo acesso(qualquer requisição).
+    // Porém o acesso está sendo restrito no endpoint.
+
+    /**
+     * Endpoint público, pode ser acessado por qualquer usuário.
+     * @return
+     */
 	@GetMapping
 	public ResponseEntity<List<ProductDTO>> findAll() {
 		List<ProductDTO> list = productService.findAll();
 		return ResponseEntity.ok(list);
 	}
-	
+
+    /**
+     * Endpoint restrito ao Admin e ao Operator.
+     * @param id
+     * @return
+     */
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
     @GetMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
         ProductDTO dto = productService.findById(id);
         return ResponseEntity.ok(dto);
     }
-	
+
+    /**
+     * Endpoint restrito somente ao Admin.
+     * @param dto
+     * @return
+     */
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
         dto = productService.insert(dto);
